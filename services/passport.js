e200c4f0866b46c9e56726d8d660d1a3429401a1
Nga <ngattt283@gmail.com>
2020-09-28 17:06:36 +0700

    react app client

--- services/passport.js
+++ services/passport.js
@@ -16,22 +16,20 @@ password.deserializeUser((id, done) => {
 })
 
 password.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({googleId: profile.id}).then(existingUser => {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        proxy: true
+    }, async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId: profile.id});
         if (existingUser) {
             //null: no error
-            done(null, existingUser);
-        } else {
-            new User({googleId: profile.id})
-                .save()
-                .then(user => done(null, user));
+            return done(null, existingUser);
         }
-    })
-}));
+        const user = await new User({googleId: profile.id}).save();
+        done(null, user);
+    }
+));
 
 
 
