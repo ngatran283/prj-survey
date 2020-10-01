04c5d0e362bc5185037d2d078a72276cd54cb5e6
Nga <ngattt283@gmail.com>
2020-10-01 15:42:18 +0700

    build client production

---
+++ client/build/precache-manifest.7ae40fa0c1068b4b12d9a8f8bada7d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfda46d433edfaed8c42d4a95e26423e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56de59319bc8503f26bd",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "56de59319bc8503f26bd",
+    "url": "/static/js/2.de0f3e36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de0f3e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810cc459c855bbf8544f",
+    "url": "/static/js/main.5463a117.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
