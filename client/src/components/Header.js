7c0b59a027a23496ba37dc67b1aa499e6d475e99
Nga <ngattt283@gmail.com>
2020-10-01 16:56:46 +0700

    Revert "bill pay"
    This reverts commit 6a6ac9efc2a3d220159d3315e9c43e52fd17c292.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,7 +13,6 @@ class Header extends Component {
             default:
                 return [
                     <li key="1"><Payment /></li>,
-                    <li key="3" style={{margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>,
                     <li key="2"><a href="/api/logout">Logout</a></li>];
         }
     }
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -7,5 +7,6 @@ export default function (state = null, action) {
         default:
             return state;
     }
+
 }
 
