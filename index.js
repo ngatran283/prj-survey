ad681b5d987e0b86e2875f1590a371ff3b14c2b3
Nga <ngattt283@gmail.com>
2020-10-29 08:31:52 +0700

    test

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 //express handle for production
 if(process.env.NODE==='production'){
-    app.use(express.static('client/build'));
     const path = require('path');
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname+'/client/build/index.html'));
-    })
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 }
 
 const PORT = process.env.PORT || 5000;
