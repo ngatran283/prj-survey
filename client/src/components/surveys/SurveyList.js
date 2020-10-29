bf7075074424c52d3cabec3313becbbbe9e746d0
Nga <ngattt283@gmail.com>
2020-10-29 08:48:05 +0700

    up

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -17,8 +17,8 @@ class SurveyList extends Component {
                         <p className="right">Sent On: {new Date(survey.dateSent).toLocaleDateString()}</p>
                     </div>
                     <div className="card-action">
-                        <a>Yes: {survey.yes}</a>
-                        <a>Yes: {survey.no}</a>
+                        <p>Yes: {survey.yes}</p>
+                        <p>Yes: {survey.no}</p>
                     </div>
                 </div>
             )
